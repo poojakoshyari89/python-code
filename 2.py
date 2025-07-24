0da9f2f02b566e2cd58d9174795c634f45009571
Pooja <koshyaripooja239@gmail.com>
2025-07-24 16:32:56 +0530

    sample file

---
+++ 2.py
@@ -0,0 +1,103 @@
+for i in range(1,6):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
+
+
+
+
+#WAP to print all the values using loop
+student = {"name":"Pooja","age":16,"subject":"AI"}
+for i in student.values():
+    print(i)
+
+print("==============================================================")
+
+
+
+'''#Delete a key value pair
+student = {"name":"Pooja","age":16,"subject":"AI"}
+student.popitem()
+print("\nAfter popitem(removes last item):")
+print(student)
+print("==============================================================")
+
+
+x,y,z = 10,20,30
+print(a,y,z)print("==============================================================")
+
+
+x,y = 25,50
+print(x,y)
+print("==============================================================")
+
+
+#write a python prrrogram that accepts radius of circle and print its area.
+
+radius=int(input("enter the radius"))
+area= '3.14*radius**2'
+print("area of circle is",area)
+print("==============================================================")
+
+#write a program to find sum of 2 no's in python
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+sum= num1+num2
+print("sum=",sum)
+print("==============================================================")
+
+
+#Update the value of an existing key in a dictionary.
+dict={"Pari":36,"School no":49,"House no":8}
+print(dict{'School no'}=45
+      print(dict)
+print("==============================================================")
+
+#Access the value associated with a given key in a dictionary.
+dict={"Pari":36,"School no":49,"House no":8}
+a=dict.keys()
+b=dict.values()
+print(a)
+print(b)
+print("==============================================================")
+
+#13 or len (13)
+a='pooja'
+print(len(a))
+print("==============================================================")
+
+# write prog. to reas l,b,h of a llgm calculate its aea and perimeter.
+
+length=float(input("enter length here"))
+breadth=float(input("enter breadth here"))
+height=float(input("enter height here"))
+perimeter=2*(length+breadth)
+print('perimeter of llgm is', perimeter)
+area=breadth*height
+print('area of llgm is ',area)
+print("==============================================================")
+
+print("Hello Tanuja")
+print("==============================================================")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
